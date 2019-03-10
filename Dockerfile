5d8132241c9f9d97af7b4ddfeb919c90739c40e6
shreyansvm <shreyans.mulkutkar@gmail.com>
2019-03-09 16:31:05 -0800

    Creating a containerized image using a simple helloWorld C++ program and Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM scratch
+ADD helloWorldC /
+CMD ["/helloWorldC"]
---
+++ helloWorldC.cc
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main() {
+	cout << "###############################################################\n";
+	cout << "\tHello from basic C++ HelloWorld Dockerized image \n ";
+	cout << "###############################################################\n";
+	return 0;
+}
